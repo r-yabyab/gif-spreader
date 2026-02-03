d15bf654626e66ad5f728689b133edfa4434b83d
Scruffy pc <cayabyabrr@yahoo.com>
2026-02-03 02:07:54 -0600

    open gif

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from PIL import Image
 
-def main() {
+def main():
+    pass
 
-}
-
-def readGif() {
-    
-}
+if __name__ == "__main__":
+    gif = Image.open('gifs/nyan-cat-rainbow.gif')
+    print(f"Loaded gif: {gif.format} {gif.size} ({gif.n_frames} frames)")
